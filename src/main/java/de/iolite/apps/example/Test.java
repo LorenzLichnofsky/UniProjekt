6988a19448c8741494f125ba68146340e697c24b
LL <lorenz.lichnofsky@web.de>
2017-01-05 23:23:14 +0100

    Beispiel Applet

--- src/main/java/de/iolite/apps/example/Test.java
+++ src/main/java/de/iolite/apps/example/Test.java
@@ -1,40 +1,29 @@
 package de.iolite.apps.example;
 
 import java.applet.Applet;
-import java.awt.Color;
-import java.awt.Font;
-import java.awt.Graphics;
+
+import java.awt.*;
 
 public class Test extends Applet {
-	
-//	  Font   f1 = new Font( "Helvetica", Font.BOLD, 11 );
-//	  Font   f2 = new Font( "Helvetica", Font.BOLD, 48 );
-//	  Color  c1 = new Color( 255, 0, 0 );
-//	  Color  c2 = new Color( 0, 0, 255 );
-	  String s;
 
-	  public void init()
-	  {
-		  s = getParameter( "Zeichenkette" );
-		  System.out.println(s);
-	  }
+    public void init(){
 
-	  public void paint( Graphics g )
-	  {
-		  
-		  g.drawString("Hello World", 50, 25);
-//	    g.setColor( c1 );
-//	    if( null == s || 0 >= s.length() ) {
-//	      g.setFont( f1 );
-//	      g.drawString( "Keine 'Zeichenkette' gefunden: " +
-//	        "wahrscheinlich wurde Applet ohne HTML-Datei gestartet.",
-//	        50, 65 );
-//	    } else {
-//	      g.setFont( f2 );
-//	      g.drawString( s, 50, 65 );
-//	    }
-//	    g.setColor( c2 );
-//	    g.drawOval( 20, 0, 205, 95 );
-	  }
+    }
+     
+     public void stop(){
+    	 
+     }
+     
+     public void paint(Graphics g){
 
+         g.setColor(Color.BLACK);
+         g.fillRect(0, 0, 400, 400);
+         
+         g.setColor(Color.GREEN);
+         g.drawRect(20, 20, 30, 50);
+
+         g.setColor(Color.RED);
+         g.fillOval(60, 70, 20, 50);
+         
+     }
 }
