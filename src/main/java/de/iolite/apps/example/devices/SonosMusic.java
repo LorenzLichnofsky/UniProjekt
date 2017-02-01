3144cb3ae024f0dade1c816f0cf6380fe07752e3
asiemund <ASiemund@googlemail.com>
2017-02-01 14:04:15 +0100

    Update SonosMusic.java
    Final Version of SonosMusic Code.

--- src/main/java/de/iolite/apps/example/devices/SonosMusic.java
+++ src/main/java/de/iolite/apps/example/devices/SonosMusic.java
@@ -1,83 +1,23 @@
 package de.iolite.apps.example.devices;
 
+import java.util.concurrent.TimeUnit;
+
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import de.iolite.app.api.device.DeviceAPIException;
 import de.iolite.app.api.device.access.Device;
-import de.iolite.app.api.device.access.DeviceBooleanProperty;
-import de.iolite.app.api.device.access.DeviceBooleanProperty.DeviceBooleanPropertyObserver;
-import de.iolite.app.api.device.access.DeviceProperty;
 import de.iolite.app.api.device.access.DeviceStringProperty;
-import de.iolite.app.api.device.access.DeviceStringProperty.DeviceStringPropertyObserver;
 import de.iolite.drivers.basic.DriverConstants;
-import de.iolite.drivers.basic.DriverConstants.PlaybackState;
+import de.iolite.utilities.concurrency.scheduler.Scheduler;
 
 public class SonosMusic {
 	
 	private static final Logger LOGGER = LoggerFactory.getLogger(SonosMusic.class);
 	
-//	public void turnSonosOn (final Device device){
-//
-//		final DeviceBooleanProperty onProperty = device.getBooleanProperty(DriverConstants.PROFILE_PROPERTY_MediaPlayerDevice_on_ID);
-//		
-//		if (onProperty != null){
-//			final boolean isDeviceOn = onProperty.getValue();
-//			
-//			if (isDeviceOn == false){
-//				
-//				try {
-//					onProperty.requestValueUpdate(true);
-//					onProperty.setObserver(new DeviceBooleanPropertyObserver() {
-//
-//						@Override
-//						public void valueChanged(Boolean value) {
-//							
-//							if (value) {
-//								LOGGER.info("Sonos turned on.");
-//								//playMusic(device);
-//							}
-//							else {
-//								LOGGER.info("Sonos still off.");
-//							}
-//							
-//						}
-//
-//						@Override
-//						public void deviceChanged(Device device) {
-//							// TODO Auto-generated method stub
-//							
-//						}
-//
-//						@Override
-//						public void keyChanged(String key) {
-//							// TODO Auto-generated method stub
-//							
-//						}
-//						
-//					});
-//					
-//				} catch (DeviceAPIException e) {
-//					// TODO Auto-generated catch block
-//					e.printStackTrace();
-//				}
-//			} else {
-//				LOGGER.info("Sonos is already on.");
-//			}
-//			
-//		} else {
-//			LOGGER.info("Property not found!");
-//		}
-//		
-//		
-//	}
-	
-	public void playMusic (final Device device){
-		
-//		for (DeviceProperty property: device.getProperties()){
-//			LOGGER.debug("{}", property.getKey());
-//		}
-//		
+
+	public void playMusic (final Device device, Scheduler scheduler){
+
 		String PLAY = "play";
 		
 		final DeviceStringProperty playMusicProperty = device.getStringProperty(DriverConstants.PROFILE_PROPERTY_MediaPlayerDevice_playbackState_ID);
@@ -85,79 +25,89 @@ public class SonosMusic {
 		
 		if (playMusicProperty != null){
 			final String musicStatus = playMusicProperty.getValue();
-			LOGGER.debug("Der Status ist: '{}'", musicStatus);
 			
 			if (musicStatus != "play"){
+				
 				try {
+					
 					playMusicProperty.requestValueUpdateFromString(PLAY);
-					playMusicProperty.setObserver(new DeviceStringPropertyObserver() {
-
-						@Override
-						public void deviceChanged(Device device) {
-							// TODO Auto-generated method stub
-						}
-
-						@Override
-						public void keyChanged(String key) {
-							// TODO Auto-generated method stub
-						}
-
-						@Override
-						public void valueChanged(String status) {
-							LOGGER.debug("Sonos turned status to {}.", musicStatus);
-						}
-						
-					});
 					
 				} catch (DeviceAPIException e) {
 					// TODO Auto-generated catch block
 					e.printStackTrace();
 				}
+				
+				TimeUnit sec = TimeUnit.SECONDS;
+				scheduler.schedule(stopMusic(device), 15, sec);
+				
 			} else {
 				LOGGER.debug("Current Status is already play.");
 			}
 			
 		} else {
 			LOGGER.info("PlayMusicProperty not found!");
-		}
+		}	
 	}
 	
-	public void addSong (final Device device){
+	public Runnable stopMusic (final Device device){
+		
+	return new Runnable() {
+	
+	@Override
+	public void run() {
+		
+		String STOP = "stop";
+		
+		final DeviceStringProperty stopMusicProperty = device.getStringProperty(DriverConstants.PROFILE_PROPERTY_MediaPlayerDevice_playbackState_ID);
+
+		
+		if (stopMusicProperty != null){
+			final String musicStatus = stopMusicProperty.getValue();
+			
+			if (musicStatus != "stop"){
+				
+				try {
+					
+					stopMusicProperty.requestValueUpdateFromString(STOP);
+					
+				} catch (DeviceAPIException e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				}
+				
+			} else {
+				LOGGER.debug("Current Status is already stop.");
+			}
+			
+		} else {
+			LOGGER.info("PlayMusicProperty not found!");
+		}
+	}
+	};
+}
+	
+	
+	public void addSong (final Device device, Scheduler scheduler){
 		
 		 final DeviceStringProperty song = device.getStringProperty(DriverConstants.PROFILE_PROPERTY_MediaPlayerDevice_mediaURI_ID);
 		 
 		 if (song != null){
 			 
 			 try {
+				 
 				song.requestValueUpdate("http://downloads.hendrik-motza.de/river.mp3");
-				song.setObserver(new DeviceStringPropertyObserver(){
-
-					@Override
-					public void deviceChanged(Device device) {
-						// TODO Auto-generated method stub
-						
-					}
-
-					@Override
-					public void keyChanged(String key) {
-						// TODO Auto-generated method stub
-						
-					}
-
-					@Override
-					public void valueChanged(String value) {
-						LOGGER.info("Habe die URI geändert in: {}", value);
-						playMusic(device);
-						
-					}
-					
-				});
+				
 			} catch (DeviceAPIException e) {
 				// TODO Auto-generated catch block
 				e.printStackTrace();
-			}				
+			}
+			 
+				playMusic(device, scheduler);
 		 
-		 }
+		 } else {
+				LOGGER.info("Song Property not found!");
+		 	}
 	}
 }
+
 
