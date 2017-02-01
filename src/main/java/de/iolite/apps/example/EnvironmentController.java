10180f1685370e13f2848c2202cae424dd36f5d6
asiemund <ASiemund@googlemail.com>
2017-02-01 14:26:47 +0100

    Create EnvironmentController.java
    Still need to create the controller Package in eclipse

---
+++ src/main/java/de/iolite/apps/example/EnvironmentController.java
@@ -0,0 +1,32 @@
+package de.iolite.apps.example.controller;
+
+import de.iolite.app.api.environment.EnvironmentAPI;
+
+/** Handles the Current Situation of the user. */
+public class EnvironmentController {
+	
+	EnvironmentAPI environmentAPI;
+
+	/** 
+	 * Constructor with the environmentAPI that is initialized in the startHook() Method of the Main Class.
+	 * @param environmentAPI 
+	 * @see CalendarIntegrationAppMain
+	 */
+	public EnvironmentController (EnvironmentAPI environmentAPI){
+		this.environmentAPI = environmentAPI;
+	}
+	
+	/** 
+	 * Checks, whether the user is at home or not.
+	 * @return Boolean value of the users current situation.
+	 */
+	public boolean isUserAtHome (){
+		
+		if (this.environmentAPI.getCurrentSituationIdentifier().equals("https://dev.iolite.de/situation/type#AtHome")){
+			return true;
+		} else {
+			return false;
+		}
+		
+	}
+}
