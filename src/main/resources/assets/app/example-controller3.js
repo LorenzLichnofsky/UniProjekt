b4f04666e4f1345c3ddebb257c4dde9499ee5cf2
LL <lorenz.lichnofsky@web.de>
2017-02-14 14:50:01 +0100

    Fix interface bug with storage API

--- src/main/resources/assets/app/example-controller3.js
+++ src/main/resources/assets/app/example-controller3.js
@@ -30,6 +30,26 @@
 	        value: '',
 		    id: 'mirror-4'
 	    }];
+		
+		ModelAPIProfiles.get(ModelAPIProfiles.storageId, {
+			success: function(storageAPI) {
+				console.log("Test: get success for function 1 of mirror_calendar toggle");
+				storageAPI.action({
+					request: new ActionRequest(null, null, ".", "loadString", [new ValueParameter("Mirror_Calendar")]),
+					success: function(calendar_status) {
+						console.log('Mirror_Calendar: '+ calendar_status);
+						var calendar_status_as_bool = (calendar_status === 'true');
+						$scope.checkedElements['mirror-4'] = calendar_status_as_bool;
+						$scope.$apply();
+					},
+					error: function(storageAPI, responseRequestID, responseErrorCode, responseError) {
+						console.log("Requesting value update of ", device, " failed due to ",
+							responseErrorCode, ": ", responseError);
+					}
+				});
+
+			}
+		});
 
 	    
 	    // storage functions for checkboxes
